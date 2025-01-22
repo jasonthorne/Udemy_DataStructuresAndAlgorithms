e634b5125d67cd080e988011be4bc0fe21f980bd
jasonthorne <jasonthorne@live.ie>
2025-01-22 17:04:12 +0000

    calculator

--- traversy-js-challenges/01-basic-challenges-1/03-calculator/calculator.js
+++ traversy-js-challenges/01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,26 @@
-function calculator() {}
+function calculator(num1,num2, op) {
+    try{
+
+        if(isNaN(num1)) throw "num1 is not a number";
+        if(isNaN(num2)) throw "num2 is not a number";
+        if(!['+','-','/','*'].includes(op)) throw "Invalid operator";
+
+        switch(op){
+            case '+':
+                return num1+num2;
+            case '-':
+                return num1-num2;
+            case '/':
+                return num1/num2;
+            case '*':
+                return num1*num2;
+            default:
+                throw "Invalid opperation"
+        }
+
+    }catch(error){
+        console.log(error);
+    }
+}
 
 module.exports = calculator;
