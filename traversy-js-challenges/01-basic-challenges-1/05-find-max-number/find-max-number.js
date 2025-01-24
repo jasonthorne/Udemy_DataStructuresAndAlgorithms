0a0b8cf81ee94bace2def4ae30dd3b316201c35a
jasonthorne <jasonthorne@live.ie>
2025-01-24 22:29:53 +0000

    max number

--- traversy-js-challenges/01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ traversy-js-challenges/01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(numbers) {
+    let maxNum = 0;
+    for (const number of numbers) {
+        if (number > maxNum){
+            maxNum = number;
+        }
+    }
+    return maxNum;
+}
 
 module.exports = findMaxNumber;
