9af4af0e5a356288a624ff804317dba8a7f0ec3c
jasonthorne <jasonthorne@live.ie>
2025-01-26 22:53:10 +0000

    palandrome

--- traversy-js-challenges/01-basic-challenges-1/08-palindrome/palindrome.js
+++ traversy-js-challenges/01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,14 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+
+    str = str.replace(/\W/g, '').toLowerCase(); //remove non alphanumeric characters
+
+    let reversedStr = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+    
+    return str === reversedStr;
+}
 
 module.exports = isPalindrome;
