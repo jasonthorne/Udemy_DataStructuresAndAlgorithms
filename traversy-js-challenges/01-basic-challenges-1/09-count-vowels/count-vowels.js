100bc06329865d0bc1f08ebfa9fce4968579a1ff
jasonthorne <jasonthorne@live.ie>
2025-01-28 17:02:36 +0000

    count vowels

--- traversy-js-challenges/01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ traversy-js-challenges/01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,11 @@
-function countVowels() {}
+function countVowels(str) {
+    let vowelCount = 0;
+    const vowels = ['a','e','i','o','u'];
+
+    str.toLowerCase().split('').forEach(char => { //split string into arrays of individual chars
+        if(vowels.includes(char)){vowelCount++;}
+    });
+    return vowelCount;
+}
 
 module.exports = countVowels;
