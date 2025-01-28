cfd2a934b548d0f90ad11a34e138a5c462fe3086
jasonthorne <jasonthorne@live.ie>
2025-01-28 17:44:31 +0000

    remove duplicates

--- traversy-js-challenges/01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ traversy-js-challenges/01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,20 @@
-function removeDuplicates() {}
+function removeDuplicates(duplicates) {
+
+    //change set to array:
+    //https://www.scaler.com/topics/javascript-set-to-array/
+
+   
+    //longer way of doing it:
+    /*
+    set = new Set();
+
+    duplicates.forEach(element => {
+        set.add(element);
+    });
+    return [...set];
+    */
+
+    return Array.from(new Set([...duplicates]));
+}
 
 module.exports = removeDuplicates;
