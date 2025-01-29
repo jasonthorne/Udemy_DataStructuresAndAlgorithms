2eca8127b52dd2376d62f60a8fd986e24b98db5c
jasonthorne <jasonthorne@live.ie>
2025-01-29 14:56:49 +0000

    array intersection

--- traversy-js-challenges/02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ traversy-js-challenges/02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,18 @@
-function arrayIntersection() {}
+function arrayIntersection(array1, array2) {
+    try{
+        const matchedNums = new Set();
+
+        new Set(array1).forEach((num)=>{
+            if(isNaN(num) || !num>0){throw 'positive numbers only'}
+            else if(array2.includes(num)){
+                matchedNums.add(num);
+            }
+        });
+
+        return [...matchedNums];
+
+    }catch(error){console.log(error);}
+
+}
 
 module.exports = arrayIntersection;
