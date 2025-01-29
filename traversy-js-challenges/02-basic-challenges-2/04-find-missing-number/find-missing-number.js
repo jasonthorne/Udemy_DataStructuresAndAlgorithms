4c746b04fefe34e75f144a01dfc65dab5ba834d1
jasonthorne <jasonthorne@live.ie>
2025-01-29 18:16:22 +0000

    find missing number

--- traversy-js-challenges/02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ traversy-js-challenges/02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,23 @@
-function findMissingNumber() {}
+function findMissingNumber(numbers) {
+
+    try{
+
+        //if (numbers.length=0){return 1};
+        numbers = numbers.sort(function(a, b){return a-b});  //https://www.w3schools.com/jsref/jsref_sort.asp
+        let pointer = numbers[0];
+
+        for(let i=0;i<numbers.length;i++){
+            if(numbers[i] === pointer){
+                pointer = pointer+1;
+                continue;
+            }else{
+               return pointer;
+            }
+        }
+
+    }catch(error){
+        console.log(error);
+    }
+}
 
 module.exports = findMissingNumber;
