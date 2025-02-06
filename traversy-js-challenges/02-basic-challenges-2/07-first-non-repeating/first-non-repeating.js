4051179a8e44be5ffa41e16b8700a81acab644ec
jasonthorne <jasonthorne@live.ie>
2025-02-06 17:44:50 +0000

    first non repeating char

--- traversy-js-challenges/02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ traversy-js-challenges/02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,20 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+    /*
+        findFirstNonRepeatingCharacter('aabccdeff'); // should return 'b'
+        findFirstNonRepeatingCharacter('aabbcc'); // should return null
+        findFirstNonRepeatingCharacter('abcdef'); // should return 'a'
+
+        const result1 = findFirstNonRepeatingCharacter('pprogramming');
+        //const result2 = findFirstNonRepeatingCharacter('abacddbec');
+    */
+        //increment a & b by 2:
+        for(let a=0,b=1;a<str.length-1;a+=2,b+=2){ 
+
+            if(str[a] !== str[b]){ //check each batch of two positions for matching pairs
+                return str[a];
+            }
+        }
+        return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
