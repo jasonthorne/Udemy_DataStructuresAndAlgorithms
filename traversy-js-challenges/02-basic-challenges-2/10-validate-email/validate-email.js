7de9c14e892a895de37409b946a23cba6a2956cb
jasonthorne <jasonthorne@live.ie>
2025-02-09 23:54:11 +0000

    validate email

--- traversy-js-challenges/02-basic-challenges-2/10-validate-email/validate-email.js
+++ traversy-js-challenges/02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,5 +1,16 @@
 function validateEmail(email) {
 
+    /*
+    validateEmail('john@gmail.com'); // true
+    validateEmail('john@gmail'); // false
+    */
+
+    const end = '.com';
+
+    //ceck if contains @ and .com
+
+
+
 }
 
 module.exports = validateEmail;
