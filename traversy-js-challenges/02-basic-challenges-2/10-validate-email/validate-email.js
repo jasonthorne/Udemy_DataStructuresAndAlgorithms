5369734ca189e2707960d96f9def7388c1986245
jasonthorne <jasonthorne@live.ie>
2025-02-10 23:53:04 +0000

    validate email

--- traversy-js-challenges/02-basic-challenges-2/10-validate-email/validate-email.js
+++ traversy-js-challenges/02-basic-challenges-2/10-validate-email/validate-email.js
@@ -3,9 +3,25 @@ function validateEmail(email) {
     /*
     validateEmail('john@gmail.com'); // true
     validateEmail('john@gmail'); // false
+
+
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    fruits.includes("Mango");
+
+    const ages = [3, 10, 18, 20];
+    document.getElementById("demo").innerHTML = ages.some(checkAdult);
+
+    function checkAdult(age) {
+    return age > 18;
+    }
+
     */
 
-    const end = '.com';
+    const domains = ['.com', '.org'];
+    return email.includes('@') && email.endsWith(domains.some((domain)=>email.includes(domain)));
+
+
+    //return email.includes('@') && email.endsWith('.com');
 
     //ceck if contains @ and .com
 
