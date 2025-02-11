cac5977a4418e6504774413c3c2e4a8e201eeb2c
jasonthorne <jasonthorne@live.ie>
2025-02-11 00:30:35 +0000

    validate email

--- traversy-js-challenges/02-basic-challenges-2/10-validate-email/validate-email.js
+++ traversy-js-challenges/02-basic-challenges-2/10-validate-email/validate-email.js
@@ -3,30 +3,14 @@ function validateEmail(email) {
     /*
     validateEmail('john@gmail.com'); // true
     validateEmail('john@gmail'); // false
-
-
-    const fruits = ["Banana", "Orange", "Apple", "Mango"];
-    fruits.includes("Mango");
-
-    const ages = [3, 10, 18, 20];
-    document.getElementById("demo").innerHTML = ages.some(checkAdult);
-
-    function checkAdult(age) {
-    return age > 18;
-    }
-
     */
 
-    const domains = ['.com', '.org'];
-    return email.includes('@') && email.endsWith(domains.some((domain)=>email.includes(domain)));
+    const domains = ['.com', '.org', '.ie'];
 
-
-    //return email.includes('@') && email.endsWith('.com');
-
-    //ceck if contains @ and .com
-
-
-
+    //return that index of '@' is greater than 0, i.e. not at the start of string (-1 is returned if not found)
+    return email.indexOf('@')>0 && domains.some(
+        (domain) => email.endsWith(domain) //and that string ends with a domain from domains
+    );
 }
 
 module.exports = validateEmail;
