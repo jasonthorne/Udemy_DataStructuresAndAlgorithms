25887e137b6dce9b2e10d5e7f09df809d48f5cf6
jasonthorne <jasonthorne@live.ie>
2025-02-19 00:22:36 +0000

    high order array methods

--- traversy-js-challenges/03-high-order-array-methods/01-simple-examples/simple-examples.js
+++ traversy-js-challenges/03-high-order-array-methods/01-simple-examples/simple-examples.js
@@ -4,17 +4,33 @@ const numbers = [1, 2, 3, 4, 5];
  * map: Transforms array elements with a provided function, creating a new array.
  */
 
+const numbers2 = numbers.map((x, index, array)=>{ //note can pass index of each element too
+    console.log(index); //index of ith pos passed in
+    //console.log(array); //entire array is passedin
+    return x = x+2;
+});
+
+console.log(numbers2);
+
 
 /**
  * filter: Creates a new array with elements that satisfy a specified condition.
  */
 
+const numbers3 = numbers.filter((x)=>{
+    return x%2 === 0;
+});
 
+console.log(numbers3);
 
 /**
  * reduce: Accumulates array elements into a single value using a provided function.
  */
 
+const number4 = numbers.reduce((total, num) => {
+   return total + num; //total is current running total, num is val at current pos
+});
+console.log(number4);
 
 
 /**
