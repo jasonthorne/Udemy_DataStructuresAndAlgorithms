2ec47b20b4c0fe388176b64df9d55bb8026079eb
jasonthorne <jasonthorne@live.ie>
2025-02-21 22:59:59 +0000

    sum of even squares

--- traversy-js-challenges/03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ traversy-js-challenges/03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,8 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+    return numbers
+        .filter(num=>num%2===0)
+        .map(even=>even*even)
+        .reduce((total,num)=>total+num,0);
+}
 
 module.exports = sumOfEvenSquares;
