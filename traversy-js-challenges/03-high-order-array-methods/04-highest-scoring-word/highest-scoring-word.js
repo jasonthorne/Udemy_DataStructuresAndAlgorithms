1d999d7e0f4010009d5217f7571383cbc9343b9e
jasonthorne <jasonthorne@live.ie>
2025-02-22 23:55:36 +0000

    highest scoring word

--- traversy-js-challenges/03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ traversy-js-challenges/03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -2,7 +2,18 @@ function highestScoringWord(words) {
 
     //https://stackoverflow.com/questions/60141168/get-index-position-of-an-alphabet
 
-    
+    /*
+    You can use the split method to separate the words.
+    You can use the map method to calculate the score for each word.
+    You can use the reduce method to find the word with the highest score.
+    */
+
+    words.split('')
+        .map(letter=>{
+            console.log('letter',letter);
+        });
+
+
     var idx = 'a'.toLowerCase().charCodeAt(0) - 96;
     console.log(idx);
 }
