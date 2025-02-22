4946fe1651f3d9f76be7f9b7798bad75df6c2108
jasonthorne <jasonthorne@live.ie>
2025-02-22 20:02:44 +0000

    calculate total sales

--- traversy-js-challenges/03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ traversy-js-challenges/03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,8 +1,8 @@
 function sumOfEvenSquares(numbers) {
     return numbers
-        .filter(num=>num%2===0)
-        .map(even=>even*even)
-        .reduce((total,num)=>total+num,0);
+        .filter(num=>num%2===0) //filter or even numbers
+        .map(even=>even*even) //quare even numbers
+        .reduce((total,num)=>total+num,0); //return total of even numbers
 }
 
 module.exports = sumOfEvenSquares;
--- traversy-js-challenges/03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ traversy-js-challenges/03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,22 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, tax) {
+
+    /*
+    const products = [
+    { name: 'Apple', price: 0.5, quantity: 10 }, 0.5*10 = 5
+    { name: 'Banana', price: 0.3, quantity: 20 }, 0.3*20=6
+    { name: 'Orange', price: 0.6, quantity: 15 }, 0.6*15=9
+    ];
+    */
+
+    let total = 0;
+
+     //https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
+    products.forEach(product => {
+        //total+= Math.round((product.price*product.quantity) * 100) / 100;
+        total+=product.price*product.quantity;
+    });
+
+    return total + ((tax*total)/100);
+}
 
 module.exports = calculateTotalSalesWithTax;
