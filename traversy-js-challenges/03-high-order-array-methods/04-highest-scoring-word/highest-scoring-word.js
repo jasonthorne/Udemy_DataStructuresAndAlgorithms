c75d89fbac3bb56a76cee3ca8703d8066cf990ab
jasonthorne <jasonthorne@live.ie>
2025-02-22 23:00:56 +0000

    highest scoring word

--- traversy-js-challenges/03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ traversy-js-challenges/03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,10 @@
-function highestScoringWord() {}
+function highestScoringWord(words) {
+
+    //https://stackoverflow.com/questions/60141168/get-index-position-of-an-alphabet
+
+    
+    var idx = 'a'.toLowerCase().charCodeAt(0) - 96;
+    console.log(idx);
+}
 
 module.exports = highestScoringWord;
