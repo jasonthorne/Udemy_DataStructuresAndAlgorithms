6fd96f0a00a8f9301a1704c09b53c3e7aa7a056a
jasonthorne <jasonthorne@live.ie>
2025-02-23 23:49:38 +0000

    highest scoring word

--- traversy-js-challenges/03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ traversy-js-challenges/03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -2,80 +2,27 @@ function highestScoringWord(words) {
 
     //https://stackoverflow.com/questions/60141168/get-index-position-of-an-alphabet
 
-    /*
-    You can use the split method to separate the words.
-    You can use the map method to calculate the score for each word.
-    You can use the reduce method to find the word with the highest score.
-
-
-    const numbers2 = numbers.map((x, index, array)=>{ //note can pass index of each element too
-    console.log(index); //index of ith pos passed in
-    //console.log(array); //entire array is passedin
-    return x = x+2;
-
-    const number4 = numbers.reduce((total, num) => {
-    return total + num; //total is current running total, num is val at current pos
-    }, 0); //total starts at passed in 0
-    });
-});
-    */
-
-    /*words.split('')
-        .map(letter=>{
-            //console.log('letter',letter);
-            console.log(letter.toLowerCase().charCodeAt(0) - 96);
-            return letter.toLowerCase().charCodeAt(0) - 96
-        })
-        .reduce((total, current) => {
-            console.log('total+current: ', total+current);
-            return total+current;
-        },0);
-        */
-
-       /* let highestWord = '';
-
-        words.split('')
-        .map(letter =>{
-            console.log('letter: ',letter);
-            return letter.toLowerCase().charCodeAt(0) - 96
-        })
-        .reduce((total, current) => {
-            console.log('total+current: ', total+current);
-            return total+current;
-        },0);
-*/
-
-    //var idx = 'a'.toLowerCase().charCodeAt(0) - 96;
-    //console.log(idx);
-
-    const allWords = words.split(' ');
-    console.log(allWords);
-
     let highestWord = '';
-    let higestScore = 0;
+    let highestScore = 0;
 
-   
-
-    allWords.forEach(word => {
+    words.split(' ').forEach(word => {
     
-        console.log('word: ',word);
+        //console.log('word: ',word);
         let wordScore = 0;
 
         word.split('').forEach(letter=>{
-            console.log('letter is: ', letter);
-            console.log('letter score is: ', letter.toLowerCase().charCodeAt(0) - 96);
+            //console.log('letter is: ', letter);
+            //console.log('letter score is: ', letter.toLowerCase().charCodeAt(0) - 96);
             wordScore += letter.toLowerCase().charCodeAt(0) - 96;
-            console.log('wordscore is: ', wordScore);
+            //console.log('wordscore is: ', wordScore);
+            if(wordScore > highestScore){{
+                highestScore = wordScore;
+                highestWord = word;
+            }}
         });
-       // console.log(word.toLowerCase().charCodeAt(0) - 96);
-        //return letter.toLowerCase().charCodeAt(0) - 96
-    })
+    });
 
-    /*allWords.map(letter=>{
-            console.log('letter',letter);
-            console.log(letter.toLowerCase().charCodeAt(0) - 96);
-            //return letter.toLowerCase().charCodeAt(0) - 96
-    });*/
+    return highestWord;
 }
 
 module.exports = highestScoringWord;
