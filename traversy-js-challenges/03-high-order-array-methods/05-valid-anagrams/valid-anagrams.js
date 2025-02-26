832b53037cd0c716e476598f43a57f5aed04f167
jasonthorne <jasonthorne@live.ie>
2025-02-26 16:47:30 +0000

    valid anagrams

--- traversy-js-challenges/03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ traversy-js-challenges/03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,124 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+
+    //count frequencies with reduce:
+    //https://www.geeksforgeeks.org/count-frequency-of-an-array-item-in-javascript/
+
+    
+   /* const getFrequency = (arr, item) => {
+        return arr.reduce((count, x) => x === item ? count + 1 : count, 0);
+    };*/
+
+
+    const getFrequency = (arr, item) => {
+        return arr.reduce((count, x) => x === item ? count + 1 : count, 0);
+    };
+
+
+    const getFrequencyArray = (str) => {
+        let strArray = str.split('').sort();
+
+        const frequenciesArray = Array.from(new Set([...strArray])).map(char =>{
+            return strArray.reduce((count, x) => x === char ? count + 1 : count, 0);
+        });
+        return frequenciesArray;
+    };
+
+
+    /*
+    const a = [1, 2, 3, 2, 1, 2, 3, 1];
+    console.log(frequency(a, 2)); 
+
+
+    new Set([...duplicates]
+    */
+
+
+   
+
+    /*const getFrequency = (array)=>{
+
+        const frequencies = [];
+        new Set([...array]).forEach(char => {
+            //console.log(char);
+            //console.log(array);
+            //return array.reduce((count, x) => x === item ? count + 1 : count, 0);
+            ////arr.reduce((count, x) => x === item ? count + 1 : count, 0);
+            let argh = array.reduce((count, x) => x === 'p' ? count + 1 : count, 0);
+            console.log(argh);
+            //frequencies.push(array.reduce((count, x) => x === char ? count + 1 : count, 0));
+        });
+        return frequencies;
+    };*/
+
+    //const a = [1, 2, 3, 2, 1, 2, 3, 1];
+    //console.log(frequency(a, 2)); 
+
+    /*const str1Freqencies = [];
+
+    const myArraySet = Array.from(new Set([...str1].sort()));
+
+
+
+    myArraySet.forEach(char => {
+        str1Freqencies.push(getFrequency(str1, char));
+    });
+
+   
+
+    console.log(str1Freqencies);*/
+
+    //Array.from(new Set([...duplicates]));
+
+    
+    //console.log(getFrequency(str1, 'p')); 
+
+    //console.log(str1.split('').sort())
+
+    const a = ['1', '2', '3', '2', '1', '2', '3', '1']; /*[1, 2, 3, 2, 1, 2, 3, 1];*/
+    //console.log(getFrequency(str1.split('').sort(), 'p'))
+
+    /*str1.split('').sort().forEach(char => {
+        getFrequency(char);
+    });*/
+
+    ///////////////str1 = str1.split('').sort();
+
+    //const testArray = Array.from(new Set([...str1]));
+    
+    const str1Frequencies = [];
+
+    //testArray.forEach(char =>{
+    Array.from(new Set([...str1])).forEach(char =>{
+        console.log(getFrequency([...str1], char));
+        str1Frequencies.push(getFrequency([...str1], char));
+    });
+
+    console.log(str1Frequencies);
+
+    //-----------------------
+
+    const str1Frequencies2 = Array.from(new Set([...str1])).map(char =>{
+        return getFrequency([...str1], char);
+    });
+
+    console.log(str1Frequencies2);
+
+    //-------------------
+    console.log(getFrequencyArray(str1));
+    console.log(getFrequencyArray(str2));
+
+    /*
+
+    const numbers2 = numbers.map((x, index, array)=>{ //note can pass index of each element too
+    console.log(index); //index of ith pos passed in
+    //console.log(array); //entire array is passedin
+    return x = x+2;
+});
+
+
+
+
+    */
+}
 
 module.exports = validAnagrams;
