bc42ad2341fc04c16ed818c25c98a8125bc91ec0
jasonthorne <jasonthorne@live.ie>
2025-02-27 17:04:13 +0000

    hashtag generator

--- traversy-js-challenges/03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ traversy-js-challenges/03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,12 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+
+    if(str.trim().length === 0 || str.trim().length> 140){
+        return false;
+    }else{
+        return '#' + str.split(' ').map(word =>{
+            return word.charAt(0).toUpperCase() + word.substr(1);
+        }).join(""); //joining with empty string removes ',' added during join
+    }
+}
 
 module.exports = generateHashtag;
