984ad3a90489969134958438d114a36d87c70451
jasonthorne <jasonthorne@live.ie>
2025-03-03 23:52:36 +0000

    analise car milage

--- traversy-js-challenges/03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ traversy-js-challenges/03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -25,9 +25,26 @@ const totalMileage = cars.reduce((currTotal, currCar) => currTotal + currCar.mil
 const averageMileage = totalMileage/cars.length;
 //const highestMileageCar = cars.mileage.max();
 
-const highestMileageCar = cars.filter((car)=>{
-    return car.mileage;
+/*//add to 'frequencies' obj, letter's value as key and it's frequency as value
+frequencies[char] = strArray.reduce((count, x) => x === char ? count + 1 : count, 0);
 });
+
+let numbers = [3, 7, 2, 8, 5];
+let max = numbers.reduce((accumulator, currentValue) => {
+    return Math.max(accumulator, currentValue);
+}, numbers[0]);
+
+*/
+
+const highestMileageCar = cars.reduce((currMaxMileage, currCar)=>{
+    //return Math.max(currMileage, currCar.mileage); 
+
+    if(currCar.mileage > currMaxMileage){
+        currMaxMileage = currCar.mileage;
+        return currCar;
+    }else{return currMaxMileage}
+},0);
+
 console.log(highestMileageCar)
 //const lowestMileageCar =
 
