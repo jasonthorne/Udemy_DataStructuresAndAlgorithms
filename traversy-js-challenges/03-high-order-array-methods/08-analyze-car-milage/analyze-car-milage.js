4f127ba629f292764a36fca8b0caf54218a02148
jasonthorne <jasonthorne@live.ie>
2025-03-04 00:10:45 +0000

    analise car milage

--- traversy-js-challenges/03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ traversy-js-challenges/03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,56 +1,41 @@
 function analyzeCarMileage(cars) {
 
-/* Output:
-{  
-    averageMileage: 23333.33,
-    highestMileageCar: { make: "Honda", model: "Civic", year: 2019, mileage: 30000 },
-    lowestMileageCar: { make: "Ford", model: "Mustang", year: 2021, mileage: 15000 },
-    totalMileage: 70000
- }
-    Calculate the average mileage of all cars.
-    Find the car with the highest mileage.
-    Find the car with the lowest mileage.
-    Calculate the total mileage of all cars combined.
-*/
+    /* Output:
+    {  
+        averageMileage: 23333.33,
+        highestMileageCar: { make: "Honda", model: "Civic", year: 2019, mileage: 30000 },
+        lowestMileageCar: { make: "Ford", model: "Mustang", year: 2021, mileage: 15000 },
+        totalMileage: 70000
+    }
+        Calculate the average mileage of all cars.
+        Find the car with the highest mileage.
+        Find the car with the lowest mileage.
+        Calculate the total mileage of all cars combined.
+    */
 
-/*
-const number4 = numbers.reduce((total, num) => {
-   return total + num; //total is current running total, num is val at current pos
-}, 0); //total starts at passed in 0
+    const totalMileage = cars.reduce((currTotal, currCar) => currTotal + currCar.mileage,0);
+    const averageMileage = totalMileage/cars.length;
+    const highestMileageCar = cars.reduce((prevCar, currCar)=>{
+        //return the car withy the greatest milage:
+        if(currCar.mileage > prevCar.mileage){
+            return currCar;
+        }else{return prevCar}
 
+    },cars[0]); //start prevCar as first cart in list
 
-*/
+    const lowestMileageCar = cars.reduce((prevCar, currCar)=>{
+        //return the car withy the least milage:
+        if(currCar.mileage < prevCar.mileage){
+            return currCar;
+        }else{return prevCar}
+    },cars[0]); //start prevCar as first cart in list
 
-const totalMileage = cars.reduce((currTotal, currCar) => currTotal + currCar.mileage,0);
-const averageMileage = totalMileage/cars.length;
-//const highestMileageCar = cars.mileage.max();
-
-/*//add to 'frequencies' obj, letter's value as key and it's frequency as value
-frequencies[char] = strArray.reduce((count, x) => x === char ? count + 1 : count, 0);
-});
-
-let numbers = [3, 7, 2, 8, 5];
-let max = numbers.reduce((accumulator, currentValue) => {
-    return Math.max(accumulator, currentValue);
-}, numbers[0]);
-
-*/
-
-const highestMileageCar = cars.reduce((prevCar, currCar)=>{
-    //return Math.max(currMileage, currCar.mileage); 
-
-    console.log(currCar.mileage)
-    console.log(prevCar.mileage)
-
-    /*if(currCar.mileage > prevCar.mileage){
-        return currCar;
-    }*/
-},cars[0].mileage);
-
-console.log(highestMileageCar)
-//const lowestMileageCar =
-
-
+    return {
+        averageMileage: averageMileage,
+        highestMileageCar: highestMileageCar,
+        lowestMileageCar: lowestMileageCar,
+        totalMileage: totalMileage
+    };
 }
 
 module.exports = analyzeCarMileage;
