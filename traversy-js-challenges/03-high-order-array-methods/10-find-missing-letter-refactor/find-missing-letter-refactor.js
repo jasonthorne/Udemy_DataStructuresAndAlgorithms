34634c85221d6f5e71bac29a2d69156657d22e8e
jasonthorne <jasonthorne@live.ie>
2025-03-05 21:43:42 +0000

    find missing letter

--- traversy-js-challenges/03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ traversy-js-challenges/03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -8,20 +8,33 @@ function findMissingLetter(letters) {
     });
     */
 
-    const test = letters.sort().forEach((letter, index, array)=>{
+    const charCodes = letters.sort().map(letter => letter.charCodeAt(0));
+    console.log(charCodes)
+
+    const test = charCodes.filter((charCode, index)=>{
         //if(letter === 'c') console.log(letters[index]);
         //return letter === 'c';
         //return letter.ch
 
         //letters.charCodeAt(0)
 
-        //console.log(letter.charCodeAt(0))
+        //let testty = array[index];
 
-        if(letter.charCodeAt(0));
+       // console.log(testty)
+
+       //console.log(testty.charCodeAt(0))
+
+       if((charCode+1) !== charCodes[(index+1)]){
+            //console.log(String.fromCharCode((array[index].charCodeAt(0)+1)));
+            console.log(String.fromCharCode([charCode+1]))
+            return String.fromCharCode([charCode+1])
+        }
         
     });
 
-    console.log(test)
+   console.log(test)
+
+    ///return test;
     
     //const letterCharCodes = letters.sort().map(letter => letter)
 }
