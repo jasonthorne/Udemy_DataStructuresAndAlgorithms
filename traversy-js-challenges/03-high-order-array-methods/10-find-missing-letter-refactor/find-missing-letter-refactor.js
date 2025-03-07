f4e8b4a840b5cfc9397232afc0e0d586c6985944
jasonthorne <jasonthorne@live.ie>
2025-03-07 20:17:52 +0000

    find missing letter

--- traversy-js-challenges/03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ traversy-js-challenges/03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -10,6 +10,7 @@ function findMissingLetter(letters) {
 
     const charCodes = letters.sort().map(letter => letter.charCodeAt(0));
     console.log(charCodes)
+    console.log(letters.sort())
 
    // const test = charCodes.filter((charCode, index)=>{
         //if(letter === 'c') console.log(letters[index]);
@@ -33,10 +34,18 @@ function findMissingLetter(letters) {
 
   // console.log(test)
 
+  //callbackfn: (previousValue: number, currentValue: number, currentIndex: number, array: number[])
+
   return String.fromCharCode(charCodes
     .reduce((charCode, index)=>{
+        console.log('charCode: ', charCode)
+        console.log('index: ', index)
+        console.log('character: ', String.fromCharCode(charCode+1))
+        console.log('charCode+1: ', charCode+1)
+        console.log('charCodes: ', charCodes[(index+1)]);
+        //if((charCode+1) !== charCodes[(index+1)] && index !== (charCodes.length -1)){
         if((charCode+1) !== charCodes[(index+1)] && index !== (charCodes.length -1)){
-            //console.log(String.fromCharCode([charCode+1]));
+            console.log('found: ', String.fromCharCode([charCode+1]));
             return charCode+1;
         }
     }));
