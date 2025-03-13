7d658576b22371ec75bbdf5424b9db59639fd0b4
jasonthorne <jasonthorne@live.ie>
2025-03-13 18:04:11 +0000

    count down

--- traversy-js-challenges/04-recursion/01-recursion-intro/count-down.js
+++ traversy-js-challenges/04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,17 @@
-function countDown() {}
+function countDown(num) {
+
+    //base case
+    if(num <=0){
+        console.log("done, baby!");
+        return; //++++++++ REMEBER THIS :P
+    }
+
+    //recursive case
+    if(num >0){
+        num--
+        console.log("num is now: ", num);
+        countDown(num);
+    }
+}
 
 module.exports = countDown;
