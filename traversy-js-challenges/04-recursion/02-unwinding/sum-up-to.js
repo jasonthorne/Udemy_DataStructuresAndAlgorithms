3d99f822c2621c0e4e26591c082ab511f0c5abe5
jasonthorne <jasonthorne@live.ie>
2025-03-23 18:01:47 +0000

    unwinding

--- traversy-js-challenges/04-recursion/02-unwinding/sum-up-to.js
+++ traversy-js-challenges/04-recursion/02-unwinding/sum-up-to.js
@@ -1,3 +1,12 @@
-function sumUpTo() {}
+function sumUpTo(myNumber) {
+
+    //base case:
+    if(myNumber === 1){
+        return 1;
+    }
+
+    //recursive case:
+    return myNumber + sumUpTo(myNumber - 1);
+}
 
 module.exports = sumUpTo;
