abf40d60eb2fabb9e9d951141f8a5a68a0c7b0d9
jasonthorne <jasonthorne@live.ie>
2025-04-02 22:17:56 +0100

    array sum

--- traversy-js-challenges/04-recursion/07-array-sum/array-sum.js
+++ traversy-js-challenges/04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,19 @@
-function arraySum() {}
+function arraySum(array) {
+
+    /*
+    Think about what your base case might be with an array
+    Think about how you can break down the problem of calculating the sum of an array into smaller sub-problems.
+    You can use arr.slice(1) to create a new array excluding the first element.
+    Examples
+    arraySum([1, 2, 3, 4, 5]); // should return 15 (1 + 2 + 3 + 4 + 5 = 15)
+    arraySum([-1, -2, -3, -4, -5]); // should return -15 (-1 + -2 + -3 + -4 + -5 = -15)
+    arraySum([]); // should return 0 (empty array)
+    */
+
+    //base case:
+    
+
+
+}
 
 module.exports = arraySum;
