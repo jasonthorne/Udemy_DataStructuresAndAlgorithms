fc5981f0ea199cf65acf89b3879ed9180e8ab9f6
jasonthorne <jasonthorne@live.ie>
2025-04-03 23:07:17 +0100

    array sum

--- traversy-js-challenges/04-recursion/07-array-sum/array-sum-run.js
+++ traversy-js-challenges/04-recursion/07-array-sum/array-sum-run.js
@@ -1,5 +1,6 @@
 const arraySum = require('./array-sum');
 
 const result = arraySum([1, 2, 3, 4, 5]);
+//const result = arraySum([1]);
 
 console.log(result);
--- traversy-js-challenges/04-recursion/07-array-sum/array-sum.js
+++ traversy-js-challenges/04-recursion/07-array-sum/array-sum.js
@@ -1,4 +1,4 @@
-function arraySum(array) {
+function arraySum(arr) {
 
     /*
     Think about what your base case might be with an array
@@ -11,8 +11,12 @@ function arraySum(array) {
     */
 
     //base case:
-    
+    if(arr.length === 0){
+        return 0;
+    }
 
+    //recursive case:
+    return arr[0] + arraySum(arr.slice(1));
 
 }
 
