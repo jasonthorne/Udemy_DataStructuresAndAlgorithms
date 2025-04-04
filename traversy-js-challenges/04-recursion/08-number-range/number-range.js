83fb23cd465020e2c3b76569040988af2ab737b0
jasonthorne <jasonthorne@live.ie>
2025-04-04 22:22:24 +0100

    number range

--- traversy-js-challenges/04-recursion/08-number-range/number-range.js
+++ traversy-js-challenges/04-recursion/08-number-range/number-range.js
@@ -1,3 +1,18 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+
+    //base case:
+    if(startNum === endNum){
+        return [startNum];
+    }
+
+   //console.log([startNum+1, endNum].sort());
+
+    //recursive case:
+
+    //.toString().split('')
+    //return [].push(startNum +1);
+    return [startNum + numberRange(startNum+1, endNum)]//.sort();
+
+}
 
 module.exports = numberRange;
